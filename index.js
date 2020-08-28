b60d40837ff6b4021cd833392af84601561296ed
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-08-27 20:01:36 -0600

    finished the assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 ///////////////Menu Items (MVP)///////////////////
 
-// const latte = { name: "Cafe Latte", price: 4, category: "Drinks" };
-// const burger = { name: "Burger", price: 18, category: "Lunch" };
-// const breakfastBurrito = { name: "Breakfast Burrito", price: 16, category: "Breakfast" };
+const latte = { name: "Cafe Latte", price: 4, category: "Drinks" };
+const burger = { name: "Burger", price: 18, category: "Lunch" };
+const breakfastBurrito = { name: "Breakfast Burrito", price: 16, category: "Breakfast" };
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
@@ -58,7 +58,7 @@ const reviews = [{ name: "Daniela", rating: 5, feedback: "Beautiful atmosphere a
 
 // Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
 // reviews[7].feedback = "This place is chill with really cool people, great for getting work done on weekdays";
-// console.log(reviews);
+// console.log([7].feedback);
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
  Your function should take two arguments:
@@ -71,12 +71,10 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
-function getReviewByIndex(reviews, index) {
-    if (reviews[index] === []) {
-        return inxex[]
-    }
-}
-console.log(index[.feedback]);
+// function getReviewByIndex(reviews, index) {
+//     return reviews[index].feedback;
+// }
+// console.log(getReviewByIndex(reviews, 5));
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
 getLastReview should accept:
@@ -86,10 +84,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview( /* code here */ ) {
-    /* code here */
-}
 
+// function getLastReview(reviews) {
+//     let last = reviews[reviews.length - 1];
+//     return last.feedback;
+// }
+// console.log(getLastReview(reviews));
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
 /** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept: 
@@ -104,8 +104,8 @@ function getLastReview( /* code here */ ) {
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
-function getReviewByRating( /* code here */ ) {
-    /* code here */
+// function getReviewByRating( /* code here */ ) {
+/* code here */
 }
 
 
@@ -122,9 +122,9 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-function getLongReviews( /* code here */ ) {
-    /* code here */
-}
+// function getLongReviews( /* code here */ ) {
+//     /* code here */
+// }
 
 
 /* STRETCH 3:  This challenge is not related to the data above! 
@@ -145,7 +145,7 @@ The returned object should have the following characteristics:
 */
 
 
-function carMaker( /* code here */ ) {
-    /* code here */
+// function carMaker( /* code here */ ) {
+//     /* code here */
 
 }
